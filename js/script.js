0cc31fd0a3f21ab6319fd822b85145ac3d5ffb7c
syauqiramadhan <135190823+syauqiramadhan@users.noreply.github.com>
2023-06-01 16:34:26 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,44 +18,44 @@ document.addEventListener('click', function(e) {
 
 
 // form wa
-$(document).on('click','.send_form', function(){
-    var input_gerai = document.getElementById('wa_email');
+// $(document).on('click','.send_form', function(){
+//     var input_gerai = document.getElementById('wa_email');
     
-    /* Whatsapp Settings */
-    var walink = 'https://web.whatsapp.com/send',
-        phone = '6281211078340',
-        walink2 = 'Halo saya ingin menanyakan produk  yang ada di gerai adam',
-        text_yes = 'Terkirim.',
-        text_no = 'Isi semua Formulir lalu klik Send.';
+//     /* Whatsapp Settings */
+//     var walink = 'https://web.whatsapp.com/send',
+//         phone = '6281211078340',
+//         walink2 = 'Halo saya ingin menanyakan produk  yang ada di gerai adam',
+//         text_yes = 'Terkirim.',
+//         text_no = 'Isi semua Formulir lalu klik Send.';
     
-    /* Smartphone Support */
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    var walink = 'whatsapp://send';
-    }
+//     /* Smartphone Support */
+//     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//     var walink = 'whatsapp://send';
+//     }
     
-    if("" != input_gerai.value){
+//     if("" != input_gerai.value){
     
-     /* Call Input Form */
-    var input_select1 = $("#wa_select :selected").text(),
-        input_name1 = $("#wa_name").val(),
-        input_email1 = $("#wa_email").val(),
-        input_number1 = $("#wa_number").val(),
-        // input_url1 = $("#wa_url").val(),
-        // input_textarea1 = $("#wa_textarea").val();
+//      /* Call Input Form */
+//     var input_select1 = $("#wa_select :selected").text(),
+//         input_name1 = $("#wa_name").val(),
+//         input_email1 = $("#wa_email").val(),
+//         input_number1 = $("#wa_number").val(),
+//         // input_url1 = $("#wa_url").val(),
+//         // input_textarea1 = $("#wa_textarea").val();
     
-    /* Final Whatsapp URL */
-    var gerai_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
-        '*Name* : ' + input_name1 + '%0A' +
-        '*Email Address* : ' + input_email1 + '%0A' +
-        '*Input Number* : ' + input_number1 + '%0A' +
-        // '*Select Option* : ' + input_select1 + '%0A' +
-        // '*URL/Link* : ' + input_url1 + '%0A' +
-        // '*Description* : ' + input_textarea1 + '%0A';
+//     /* Final Whatsapp URL */
+//     var gerai_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
+//         '*Name* : ' + input_name1 + '%0A' +
+//         '*Email Address* : ' + input_email1 + '%0A' +
+//         '*Input Number* : ' + input_number1 + '%0A' +
+//         // '*Select Option* : ' + input_select1 + '%0A' +
+//         // '*URL/Link* : ' + input_url1 + '%0A' +
+//         // '*Description* : ' + input_textarea1 + '%0A';
     
-    /* Whatsapp Window Open */
-    window.open(gerai_whatsapp,'_blank');
-    document.getElementById("text-info").innerHTML = '<span class="yes">'+text_yes+'</span>';
-    } else {
-    document.getElementById("text-info").innerHTML = '<span class="no">'+text_no+'</span>';
-    }
-    });
+//     /* Whatsapp Window Open */
+//     window.open(gerai_whatsapp,'_blank');
+//     document.getElementById("text-info").innerHTML = '<span class="yes">'+text_yes+'</span>';
+//     } else {
+//     document.getElementById("text-info").innerHTML = '<span class="no">'+text_no+'</span>';
+//     }
+//     });
